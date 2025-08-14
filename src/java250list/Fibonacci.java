7b4b507d5b684c096b1dd383d0b1d743e014193f
KuldeepKGIT <kuldeepkumar290497@gmail.com>
2025-08-15 00:21:38 +0530

    14th Aug

---
+++ src/java250list/Fibonacci.java
@@ -0,0 +1,22 @@
+package java250list;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number to set the length of the series");
+        int n = scanner.nextInt();
+        int first = 0;
+        int second = 1;
+
+        for(int i = 0; i<n; i++){
+            System.out.print(first+ " ");
+            int next = second + first;
+            first = second;
+            second = next;
+
+
+        }
+    }
+}
