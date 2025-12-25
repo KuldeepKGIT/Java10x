93af9ddf4c1a0ad92fe4882e9cddc44a23f76284
KuldeepKGIT <kuldeepkumar290497@gmail.com>
2025-12-25 20:58:22 +0530

    25 Dec

---
+++ src/practiceQuestions/PyramidStarPattern.java
@@ -0,0 +1,16 @@
+package practiceQuestions;
+
+public class PyramidStarPattern {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int k=4; k>=i;k--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
