2393dce732a246abfdac7c1a92f2cd4783b66419
KuldeepKGIT <kuldeepkumar290497@gmail.com>
2026-04-27 01:36:48 +0530

    42726

--- src/devStrings/RemoveSpecialChars.java
+++ src/devStrings/RemoveSpecialChars.java
@@ -1,4 +1,14 @@
 package devStrings;
 
+import java.util.Scanner;
+
 public class RemoveSpecialChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string!");
+        String str = scanner.nextLine();
+        String result = str.replaceAll("[^a-zA-Z0-9]","");
+        System.out.println("Output String is: "+result);
+        scanner.close();
+    }
 }
